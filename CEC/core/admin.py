cd1b10eb6947bb95b156253f390a22f7e8b65843
MarcusSilva3298 <marcussilva3298@gmail.com>
2019-10-13 15:46:53 -0300

    add projetos + valores

--- CEC/core/admin.py
+++ CEC/core/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Ensino, Evento, PropostaPedagogica, Aplicativo, Galeria, Contatos
+from .models import Ensino, Evento, PropostaPedagogica, Aplicativo, Galeria, Contatos, Valores, Projetos
 
 # Register your models here.
+admin.site.register(Projetos)
 admin.site.register(Ensino)
 admin.site.register(Evento)
 admin.site.register(PropostaPedagogica)
 admin.site.register(Aplicativo)
 admin.site.register(Galeria)
 admin.site.register(Contatos)
+admin.site.register(Valores)
